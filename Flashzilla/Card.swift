32886861fa166b447cb81e0c602d2f29e42a01bf
Raymond Chen <ahrchen@gmail.com>
2022-04-13 13:07:19 -0700

    Add CardView

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Raymond Chen on 4/13/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13 Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
